d0d3a0efb57df64896a71b696d7be02601d41ff6
fredrikastjernlof <fredrika.stjernlof@live.se>
2026-05-27 02:36:36 +0200

    Create responsive neon header with hero image

--- src/app/components/site-header/site-header.ts
+++ src/app/components/site-header/site-header.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterLink } from '@angular/router'; 
+
+import 'iconify-icon';
 
 @Component({
   selector: 'app-site-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './site-header.html',
   styleUrl: './site-header.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class SiteHeader {}
