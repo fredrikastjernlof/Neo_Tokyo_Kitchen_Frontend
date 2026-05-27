c6f3637348cac6ae8ad1639c03538ec7bdefbd45
fredrikastjernlof <fredrika.stjernlof@live.se>
2026-05-27 03:38:39 +0200

    Create footer and add styling

--- src/app/components/site-footer/site-footer.ts
+++ src/app/components/site-footer/site-footer.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+import 'iconify-icon';
 
 @Component({
   selector: 'app-site-footer',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './site-footer.html',
   styleUrl: './site-footer.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class SiteFooter {}
