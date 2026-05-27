e4a95b7a627dc1081a75a0d374095f8b200f3c5f
fredrikastjernlof <fredrika.stjernlof@live.se>
2026-05-27 03:14:09 +0200

    Create responsive main navigation

--- src/app/components/main-nav/main-nav.ts
+++ src/app/components/main-nav/main-nav.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterLink, RouterLinkActive} from '@angular/router';
+import 'iconify-icon';
 
 @Component({
   selector: 'app-main-nav',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './main-nav.html',
   styleUrl: './main-nav.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class MainNav {}
