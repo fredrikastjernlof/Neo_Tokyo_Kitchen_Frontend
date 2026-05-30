6a54dd878e05ff78996a4c7f4e002e059f114813
fredrikastjernlof <fredrika.stjernlof@live.se>
2026-05-30 22:05:32 +0200

    Add separate route for admin pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,14 @@ import { Menu } from './pages/menu/menu';
 import { Booking } from './pages/booking/booking';
 import { Contact } from './pages/contact/contact';
 import { About } from './pages/about/about';
+import { AdminLogin } from './pages/admin-login/admin-login';
 
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'menu', component: Menu },
     { path: 'booking', component: Booking },
-      { path: 'contact', component: Contact },
+    { path: 'contact', component: Contact },
     { path: 'about', component: About },
+    { path: 'admin/login', component: AdminLogin},
     { path: '**', redirectTo: '' }
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,30 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs';
 
 import { SiteHeader } from './components/site-header/site-header';
 import { SiteFooter } from './components/site-footer/site-footer';
 import { MainNav } from './components/main-nav/main-nav';
+import { AdminHeader } from './components/admin-header/admin-header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SiteHeader, SiteFooter, MainNav],
+  imports: [RouterOutlet, SiteHeader, SiteFooter, MainNav, AdminHeader],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('frontend');
+
+  isAdminRoute = signal(false);
+
+  constructor(private router: Router) {
+    this.isAdminRoute.set(this.router.url.startsWith('/admin'));
+
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.isAdminRoute.set(this.router.url.startsWith('/admin'));
+      });
+  }
 }
