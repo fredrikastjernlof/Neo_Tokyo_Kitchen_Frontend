7fc113e670320efac86136321fd175376e5d4aae
fredrikastjernlof <fredrika.stjernlof@live.se>
2026-05-31 00:34:40 +0200

    Add content and style admin dashboard overview

--- src/app/pages/admin-dashboard/admin-dashboard.ts
+++ src/app/pages/admin-dashboard/admin-dashboard.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import 'iconify-icon';
 
 @Component({
   selector: 'app-admin-dashboard',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './admin-dashboard.html',
   styleUrl: './admin-dashboard.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AdminDashboard {}
