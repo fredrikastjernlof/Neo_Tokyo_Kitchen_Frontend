7bb9a0fb4043b32ff5a5f1dbcf03d8756d5d52a4
fredrikastjernlof <fredrika.stjernlof@live.se>
2026-06-01 07:00:24 +0200

    Add Image Credits page content and styling

---
+++ src/app/pages/image-credits/image-credits.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image-credits',
+  imports: [],
+  templateUrl: './image-credits.html',
+  styleUrl: './image-credits.scss',
+})
+export class ImageCredits {}
